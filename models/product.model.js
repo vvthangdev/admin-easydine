bea0cb3df7b961cafbc57994fd676aa22fe1a42a
vvthangdev <vvthang.dev@gmail.com>
2024-10-20 20:08:56 +0700

    First commit

---
+++ models/product.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please enter product name"],
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      require: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+module.exports = Product;
