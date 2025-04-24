557bf5019e80b34205f18594906493c21ef0a2a4
Thang <vvthang.dev@gmail.com>
2025-04-24 16:20:23 +0700

    v1.1

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // https://backend-easydine.onrender.com
 // http://localhost:8080
+// http://128.199.246.55:8080
 const axiosInstance = axios.create({
   baseURL: "http://128.199.246.55:8080", // Thay đổi baseURL theo API của bạn
   timeout: 10000,
