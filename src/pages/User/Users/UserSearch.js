5322a1976d83eb61871ab82b963eb3f0eb38b180
Thang <vvthang.dev@gmail.com>
2025-06-06 17:45:50 +0700

    fix auto fill in search usr

--- src/pages/User/Users/UserSearch.js
+++ src/pages/User/Users/UserSearch.js
@@ -6,44 +6,44 @@ import { Search } from 'lucide-react'
 const UserSearch = ({ searchTerm, onSearch, onEnter }) => {
   return (
     <TextField
-      placeholder="Tìm kiếm theo tên, số điện thoại hoặc ID"
-      value={searchTerm}
-      onChange={(e) => onSearch(e.target.value)}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") {
-          onEnter?.(e.target.value)
-        }
-      }}
-      fullWidth
-      size="small"
-      autoComplete="off"
-      sx={{
-        "& .MuiOutlinedInput-root": {
-          borderRadius: 2,
-          height: 40,
-          "&:hover .MuiOutlinedInput-notchedOutline": {
-            borderColor: "#0071e3",
-          },
-          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-            borderColor: "#0071e3",
-            borderWidth: 2,
-          },
-          "& .MuiInputBase-input": {
-            fontSize: "0.875rem",
-          },
-        },
-        "& .MuiInputLabel-root.Mui-focused": {
-          color: "#0071e3",
-        },
-      }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Search size={18} color="#0071e3" />
-          </InputAdornment>
-        ),
-      }}
-    />
+  placeholder="Tìm kiếm theo tên, số điện thoại hoặc ID"
+  value={searchTerm}
+  onChange={(e) => onSearch(e.target.value)}
+  onKeyDown={(e) => {
+    if (e.key === "Enter") {
+      onEnter?.(e.target.value)
+    }
+  }}
+  fullWidth
+  size="small"
+  autoComplete="new-password" // Changed from "off" to "new-password"
+  sx={{
+    "& .MuiOutlinedInput-root": {
+      borderRadius: 2,
+      height: 40,
+      "&:hover .MuiOutlinedInput-notchedOutline": {
+        borderColor: "#0071e3",
+      },
+      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+        borderColor: "#0071e3",
+        borderWidth: 2,
+      },
+      "& .MuiInputBase-input": {
+        fontSize: "0.875rem",
+      },
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#0071e3",
+    },
+  }}
+  InputProps={{
+    startAdornment: (
+      <InputAdornment position="start">
+        <Search size={18} color="#0071e3" />
+      </InputAdornment>
+    ),
+  }}
+/>
   )
 }
 
